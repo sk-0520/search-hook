b8ed2fbf8b71c51965d57d6d112742d00cb5b318
sk <content.type.text.net@gmail.com>
2018-10-13 21:03:43 +0900

    クエリのパースと結合処理実装

--- source/scripts/redirect-google.js
+++ source/scripts/redirect-google.js
@@ -3,10 +3,19 @@
 const outputGoogle = createLogger('Google');
 
 function resistGoogle(setting) {
+
+    var notItems = setting.notItems.filter(function(i) {
+        return i.service.google;
+    }).filter(function(i) {
+        return i.word.length;
+    }).map(function(i) {
+        return i.word;
+    })
+
     // Google 登録
     browser.webRequest.onBeforeRequest.addListener(
         function(requestDetails) {
-            return requestGoogle(setting.service.google, requestDetails);
+            return requestGoogle(setting.service.google, notItems, requestDetails);
         },
         {
             urls: [
@@ -16,7 +25,7 @@ function resistGoogle(setting) {
     );
 }
 
-function requestGoogle(googleSetting, requestDetails) {
+function requestGoogle(googleSetting, notItems, requestDetails) {
 
     outputGoogle.log("Loading: " + requestDetails.url);
     outputGoogle.debug(JSON.stringify(requestDetails));
@@ -32,12 +41,6 @@ function requestGoogle(googleSetting, requestDetails) {
     var params = url.searchParams;
 
 
-    // 既に検索済みのページとして何もしない
-    if(!params.has('start')) {
-        outputGoogle.debug('ignore request');
-        return;
-    }
-
     // 検索数の指定が無ければ設定値に書き換え
     if(!params.has('num')) {
         params.append('num', googleSetting.searchCount)
@@ -48,9 +51,76 @@ function requestGoogle(googleSetting, requestDetails) {
         return;
     }
 
-    var query = params.get('q');
-    outputGoogle.debug("query: " + query);
+    // 既に検索済みのページとして何もしない
+    if(params.has('start')) {
+        outputGoogle.debug('ignore request');
+        return;
+    }
 
-    // 半角スペースで分割するが " で括られている場合の半角スペースは無視する
+    var rawQuery = params.get('q');
+    outputGoogle.debug("raw: " + rawQuery);
+    var queryItems = splitQuery(rawQuery)
+    outputGoogle.debug("items: " + queryItems);
 
+    var customQuery = makeCustomQuery(queryItems, notItems);
+    outputGoogle.debug("customQuery: " + customQuery);
+
+    var queryString = toQueryString(customQuery);
+    outputGoogle.debug("queryString: " + queryString);
+
+}
+
+function makeCustomQuery(queryItems, notItems) {
+    // 既に存在する否定ワードはそのまま、設定されていなければ追加していく
+
+    var addNotItems = notItems.concat();
+    var customQuery = [];
+    for(var i = 0; i < queryItems.length; i++) {
+        var query = queryItems[i];
+
+        if(!query.length) {
+            continue;
+        }
+
+        if(query.charAt('-')) {
+            var notWord = query.substr(1);
+            var index = addNotItems.indexOf(notWord);
+            customQuery.push(query);
+            if(index !== -1) {
+                addNotItems.splice(index, 1);
+            }
+        } else {
+            customQuery.push(query);
+        }
+    }
+
+    outputGoogle.debug('notItems: ' + notItems);
+    outputGoogle.debug('addNotItems: ' + addNotItems);
+
+    for(var i = 0; i < addNotItems.length; i++) {
+        var word = addNotItems[i];
+        customQuery.push('-' + word);
+    }
+
+    outputGoogle.debug('customQuery: ' + customQuery);
+
+    return customQuery;
+}
+
+function toQueryString(queryItems) {
+    var items = queryItems.filter(function(s) {
+        return s && s.length;
+    }).map(function(s) {
+        if(s.indexOf(' ') !== -1) {
+            if(s.charAt(0) === '-') {
+                return '-"' + s.substr(1) + '"';
+            }
+
+            return '"' + s + '"';
+        } else {
+            return s;
+        }
+    });
+
+    return items.join(' ');
 }
