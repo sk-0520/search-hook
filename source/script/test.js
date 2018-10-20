90b4769245e55b467c9cb0cf74adca517249c699
sk <content.type.text.net@gmail.com>
2018-10-21 02:01:19 +0900

    js 界隈 複雑怪奇

--- source/script/test.js
+++ source/script/test.js
@@ -1,8 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Test = /** @class */ (function () {
-    function Test() {
-    }
-    return Test;
-}());
-exports.Test = Test;
+export class Test {
+}
