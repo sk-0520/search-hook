50e2347bab05b193b0c7d55e11766f2354f953c5
sk <content.type.text.net@gmail.com>
2018-10-21 18:26:22 +0900

    おうおう、キャストどうすんねん

--- source/script/options.ts
+++ source/script/options.ts
@@ -7,15 +7,42 @@ export class Options extends shared.ActionBase {
     }
 
     public initialize() {
-        document.addEventListener('DOMContentLoaded', this.restore);
         document.querySelector('form')!.addEventListener('submit', this.save);
         
         document.querySelector('#command-engine-add-not-item')!.addEventListener('click', this.addNotItemFromInput);
         document.querySelector('#command-view-add-hide-item')!.addEventListener('click', this.addHideItemFromInput);
+
+        this.restore();
     }
 
     private restore(): void {
+        var getting = browser.storage.local.get('setting');
+        getting.then(
+            result => {
+                var baseSetting = new setting.MainSetting();
+                if(!result.setting) {
+                } else {
+                    var resultSetting = <setting.MainSetting>(result['setting']);
+                    baseSetting = shared.merge(baseSetting, resultSetting);
+                }
+                // if(!setting) {
+                //     // init
+                //     setting = baseSetting;
+                // } else {
+                //     setting = merge(baseSetting, setting);
+                // }
+                // setService(setting.service);
+                // setNotItems(setting.notItems);
+                // setHideItems(setting.hideItems);                
+            },
+            error => {
+                alert("bbb");
+                super.logger.error(error)
+            }
+        );
     }
+
+
     private save(): void {
     }
 
