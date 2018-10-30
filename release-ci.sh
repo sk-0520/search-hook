11c25031c765a3708b0c0814ff16d1eed045e0de
sk <content.type.text.net@gmail.com>
2018-10-30 20:47:19 +0900

    リビジョン出力

--- release-ci.sh
+++ release-ci.sh
@@ -2,7 +2,10 @@
 
 echo "Release CI"
 
+CURRENT_VERSION=$(git rev-parse HEAD)
+
+echo $CURRENT_VERSION
+
 git archive HEAD --worktree-attributes --output=./artifacts/source.zip
-
-
+touch ./artifacts/$CURRENT_VERSION
 
