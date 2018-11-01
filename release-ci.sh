cbc4732a23a7fe181a668a6559023eb4389e113f
sk <content.type.text.net@gmail.com>
2018-11-02 00:13:13 +0900

    しもうた、名前変えたのが影響してた

--- release-ci.sh
+++ release-ci.sh
@@ -7,13 +7,14 @@ API_TAG=https://api.bitbucket.org/2.0/repositories/sk_0520/search-hook/refs/tags
 ARTIFACTS_DIR=artifacts
 
 CURRENT_REVISION=$(git rev-parse HEAD)
-CURRENT_VERSION=$(grep version package.json | sed -r 's/\s*"version"\s*:\s*"(.*)".*/\1/')
+CURRENT_VERSION=$(grep '"version"' package.json | sed -r 's/\s*"version"\s*:\s*"(.*)".*/\1/')
 
 echo "revision: $CURRENT_REVISION"
 echo "version: $CURRENT_VERSION"
 
 # 転送用ファイルを準備
 ARCHIVE_FILE=search-hook_"$CURRENT_VERSION".zip
+echo "output file: $ARCHIVE_FILE"
 
 git archive HEAD --worktree-attributes --output="./$ARTIFACTS_DIR/source.zip"
 touch "./$ARTIFACTS_DIR/$CURRENT_REVISION"
